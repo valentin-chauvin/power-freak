0faf4304e219dc3d66ac8167167e7694036dfe77
valentin-chauvin <valentin.chauvin74000@gmail.com>
2019-03-08 15:43:40 +0100

    All

--- public/partials/recyclage-content.php
+++ public/partials/recyclage-content.php
@@ -1,2 +1,143 @@
 <div class="recyclage_banner">
+  <div>
+    <h2>Le recyclage<em>,</em> une priorité</h2>
+  </div>
+</div>
+
+<div class="block_julius">
+  <img class="julius_portrait" src="assets/images/julius_robert_von_mayer.png" alt="Portrait Julius Robert Von Mayer" width="300" />
+  <h3>" L’energie ne se perd jamais, elle se transmet. "</h3>
+  <img class="quotes" src="assets/images/icons/quotes.svg" alt="Quotes Icon" width="300" />
+</div>
+
+<div class="container">
+  <div class="recyclage_intro">
+    <p>La production et la distribution de Power Freak en Suisse nécessitent
+      évidemment de l'énergie, ce qui a des répercussions sur
+      l'environnement. La société Power Freak est tout à fait consciente
+      de sa responsabilité en matière de protection de l'environnement
+      et s'attache à réaliser constamment des améliorations dans ce domaine.
+      <br />
+      <br />
+      Découvrez les efforts déployés par Power Freak pour réduire
+      au maximum son empreinte carbone tout au long du cycle de vie d’une
+      canette.
+    </p>
+  </div>
+</div>
+
+<div class="recyclage_section_1">
+  <div class="recyclage_section_1_part_1">
+    <section>
+      <h4>Les canettes power freak</h4>
+      <p>
+        Power Freak a délibérément choisi d'utiliser des canettes en
+        aluminium. Aujourd’hui, leur poids a diminué significativement
+        comparé aux années précédentes, ce qui permet de préserver de
+        précieuses matières premières. Une fois collectées, elles sont
+        entièrement recyclées, à l'infini, sans aucune perte de qualité.
+      </p>
+    </section>
+
+    <div class="recyclage_section_1_image">
+      <img src="assets/images/recyclable_symbol.svg" alt="Symbole Recyclable" width="300" />
+    </div>
+  </div>
+
+  <h3>" Le recyclage d'une canette necessite 95% d'energie en moins que la production d'une nouvelle canette. "</h3>
+
+  <details>
+    <summary>En savoir plus</summary>
+    <section>
+      <h4>LA CANETTE, UNE "RESERVE D'ENERGIE"</h4>
+      <p>
+        Toutes les canettes sont recyclables à 100 % : une fois
+        collectées et parvenues aux sites de recyclage, pas le moindre
+        gramme d'aluminium ne se perd.  On peut la faire fondre et la
+        réutiliser à l'infini sans aucune perte de qualité. Le recyclage
+        de l'aluminium récupéré permet d'économiser 95 % d'énergie par
+        rapport à l'utilisation d'aluminium issu des réserves naturelles.
+        Aujourd’hui, le poids d'une canette de Power Freak a diminué
+        significativement comparé aux années précédentes. La diminution
+        constante du poids fait aussi la différence : moins on utilise
+        d'aluminium dans les canettes en début de cycle, plus petite
+        sera l'empreinte carbone.
+      </p>
+    </section>
+
+    <section>
+      <h4>CHAQUE CANETTE COMPTE</h4>
+      <p>
+        Les canettes sont les emballages de boisson les plus recyclés
+        au monde. Bien évidemment, ceci n'est possible que si les
+        canettes sont récupérées et (ré)introduites dans la boucle de
+        recyclage. Naturellement, si on laisse négligemment une canette
+        dans la nature, elle ne peut pas réaliser son potentiel de
+        recyclage. Malheureusement, dans certains pays, les détritus
+        sont encore jetés n’importe où. Jeter la canette dans une
+        poubelle ou un bac de tri, contribue à préserver la beauté de
+        nos paysages, à améliorer les taux de recyclage, à réduire la
+        consommation d'énergie ainsi que l'épuisement des ressources
+        naturelles. Si votre pays dispose d'une filière de recyclage
+        de l'aluminium, jetez votre canette de manière appropriée.
+      </p>
+    </section>
+  </details>
+</div>
+
+<div class="recyclage_section_2">
+  <div class="recyclage_section_2_part_1">
+    <section>
+      <h4>La production en "wall to wall"</h4>
+      <p>
+        Outre l'utilisation à 80% d'énergies renouvelables, la
+        fabrication de Power Freak Energy Drink s'appuie sur le
+        principe du « wall to wall ». Cela signifie que la production
+        et le remplissage ont lieu sur le même site, ce qui fait des
+        kilomètres de transport économisés. Étant donné que nous
+        n'avons pas à transporter les canettes vides jusque dans
+        l'usine de remplissage, l'empreinte écologique est
+        significativement réduite !
+      </p>
+    </section>
+
+    <div class="recyclage_section_2_image">
+      <img src="assets/images/recyclable_symbol.svg" alt="Symbole Recyclable" width="300" />
+    </div>
+  </div>
+
+  <h3>" La production en "wall to wall" permet d'economiser chaque annee l'equivalent en emissions de plus de 2,3 millions de kilometres de voyage en camion. "</h3>
+
+  <details>
+    <summary>En savoir plus</summary>
+    <section>
+      <h4>QUE CELA REPRESENTE-T-IL EN TERMES DE CHIFFRES ?</h4>
+      <p>
+        La production en « wall to wall » permet d'économiser plus
+        de 1 643 tonnes d'émissions de CO2 par an.
+      </p>
+    </section>
+
+    <section>
+      <h4>UNE PLATEFORME DE PRODUCTION DESSERVANT LA PLANETE</h4>
+      <p>
+        La production de Power Freak basée en Suisse offre de nombreux
+        avantages :
+        <br />
+        Notre processus de fabrication de pointe ainsi que la qualité
+        des ingrédients que nous employons, comme l'eau des Alpes
+        d'origine locale, nous permet de garantir aux consommateurs
+        une qualité optimale des produits.
+        <br />
+        Il est possible de mettre en œuvre rapidement de nouvelles
+        technologies permettant d’accroître l’efficacité et, par la
+        même occasion, de préserver les ressources naturelles.
+        <br />
+        L'énergie utilisée pour la production est produite dans la
+        région, elle provient à 80 % environ d'énergies renouvelables.
+        Nous cherchons à améliorer ce chiffre par tous les moyens
+        possibles.
+      </p>
+    </section>
+  </details>
 </div>
