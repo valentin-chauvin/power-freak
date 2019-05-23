6e2779f2072e40a2fa71fa7654d55c7985346a5e
valentin-chauvin <valentin.chauvin74000@gmail.com>
2019-05-23 16:57:03 +0200

    All

--- public/partials/page-head.php
+++ public/partials/page-head.php
@@ -7,24 +7,5 @@
     <div class="block_panier">
       <a href="#" title="Shop"><img src="assets/images/icons/shop.svg" alt="Icône Shop" width="30" /></a>
     </div>
-
-    <input type="checkbox" id="nav-display">
-
-    <label for="nav-display" class="block_menu_icon">
-      <div class="menu_icon">
-        <span class="line"></span>
-        <span class="line"></span>
-        <span class="line"></span>
-      </div>
-    </label>
   </div>
-</div>
-
-<div class="menu_overlay">
-  <ul>
-    <li><a href="index.php" title="Accueil">Accueil</a></li>
-    <li><a href="recyclage.php" title="Le recyclage, une priorité">Le recyclage, une priorité</a></li>
-    <li><a href="about.php" title="Power Freak">Power Freak</a></li>
-    <li><a href="contact.php" title="Contactez-nous">Contactez-nous</a></li>
-  </ul>
 </div>
